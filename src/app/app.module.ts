57b8e7444c6ebed8cdd245ee0e63c5a5a779c618
whoisrashed <learnwithrash3d@gmail.com>
2023-04-04 13:38:52 +0500

    base application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent,
   ],
   imports: [
     BrowserModule
